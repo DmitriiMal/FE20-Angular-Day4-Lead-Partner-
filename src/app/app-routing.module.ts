adc882665c1a5d613bda442e99413d934073ed25
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-10-19 12:39:16 +0200

    add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: AboutPageComponent,
   },
 ];
 
